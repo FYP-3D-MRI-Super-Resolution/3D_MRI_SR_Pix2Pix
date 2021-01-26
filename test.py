a8b19ed328ca804ba78aa9fc7d639d5d93034901
Giulia Baldini <giulia.baldini.11@gmail.com>
2021-01-26 15:47:56 +0100

    Change order of target shape

--- test.py
+++ test.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
             query_name_noext = query_name.split(".")[0]
             if 'real_B' in np_post:
                 # Evaluate the results
-                exc_eval.evaluate(np_post, query_name, opt.smoothing)
+                exc_eval.evaluate(np_post, query_name_noext, opt.smoothing)
             save_nifti_images(np_post, query_name_noext, opt, web_dir, dataset.dataset.affine)
             save_web_nifti(webpage, np_post, img_path, opt.show_plots, width=opt.display_winsize,
-                           chosen_slice=opt.chosen_slice, target_shape=(180, 210))
+                           chosen_slice=opt.chosen_slice, target_shape=(210, 180))
         else:
             save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
     webpage.save()  # save the HTML
