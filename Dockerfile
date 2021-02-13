cf106ff835edef6a2942227c024ae190a57580ef
Giulia Baldini <giulia.baldini.11@gmail.com>
2021-02-13 12:43:37 +0100

    Move command to bash file

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
 FROM nvcr.io/nvidia/cuda:11.2.1-cudnn8-runtime-ubuntu20.04
 
-ADD . .
-
 # Fix for running cv2: ffmpeg libsm6 libxext6
 ENV TZ=Europe/Berlin
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update
 RUN apt-get install python3-dev python3-pip ffmpeg libsm6 libxext6 -y
 
+WORKDIR Pix2PixNIfTI
+ADD . .
+
 RUN pip3 install -r requirements.txt
 
-ENTRYPOINT ["python3", "train.py", \
-"--dataset_mode", "nifti", \
-"--model", "pix2pix3d", \
-"--name", "t1t2", \
-"--n_epochs", "200", \
-"--n_epochs_decay", "200", \
-"--suffix", "{model}_{batch_size}_{lambda_L1}_{lambda_L2_T}" \
-]
+CMD ["bash", "grid_search.sh"]
--- grid_search.sh
+++ grid_search.sh
@@ -2,14 +2,14 @@ b_sizes=(1 5 10)
 lone=(0 100 500)
 ltwo=(0 100 500)
 
-
-python3 ./datasets/make_nifti_dataset.py --save_folder ./datasets/braindata --folder ../braindata_all/ --phase train
+# python3 ./datasets/make_nifti_dataset.py --save_folder ./datasets/braindata --folder ../braindata_all/ --phase train
 
 for b in ${b_sizes[@]}; do
   for l in ${lone[@]}; do
     for ll in ${ltwo[@]}; do
-      nvidia-docker run pix --dataroot ./datasets/braindata/ --batch_size $b --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0
+      python3 train.py --dataset_mode nifti --model pix2pix3d --name t1t2 --n_epochs 200 --n_epochs_decay 200 \
+      --suffix {model}_{batch_size}_{lambda_L1}_{lambda_L2_T} \
+      --dataroot ./datasets/braindata/ --batch_size $b --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0,1,2,3 > ./checkpoints/out_$b_$l_$ll.txt
     done
   done
 done
-
