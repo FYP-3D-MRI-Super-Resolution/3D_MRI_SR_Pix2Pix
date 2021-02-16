061a68dce3edb793632f68d14423eb9783cebc57
Giulia Baldini <giulia.baldini.11@gmail.com>
2021-02-16 16:15:09 +0100

    Remove batch size from training file names

--- grid_search.sh
+++ grid_search.sh
@@ -6,7 +6,9 @@ ltwo=(0 100 500)
 for l in ${lone[@]}; do
   for ll in ${ltwo[@]}; do
     python3 train.py --dataset_mode nifti --model pix2pix3d --name t1t2 --n_epochs 200 --n_epochs_decay 200 \
-    --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0,1,2,3 --suffix {model}_{batch_size}_{lambda_L1}_{lambda_L2_T} \
-    --dataroot /input --checkpoints_dir /checkpoints > "/checkpoints/out_${b}_${l}_${ll}.txt"
+    --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0,1,2,3 --suffix {model}_{lambda_L1}_{lambda_L2_T} \
+    --dataroot /input --checkpoints_dir /checkpoints > "/checkpoints/out_${l}_${ll}.txt"
   done
 done
+
+nvidia-docker run --mount type=bind,source=/raid/gbaldini/pix_data,target=/input --mount type=bind,source=/raid/gbaldini/pix_checkpoints,target=/checkpoints pix
