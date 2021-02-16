a56ae53777b469ef40fceedf508ab1e68511f0fc
Giulia Baldini <giulia.baldini.11@gmail.com>
2021-02-16 16:08:09 +0100

    Modify grid search for mounted directories in docker

--- grid_search.sh
+++ grid_search.sh
@@ -1,12 +1,12 @@
 lone=(0 100 500)
 ltwo=(0 100 500)
 
-# python3 ./datasets/make_nifti_dataset.py --save_folder ./datasets/braindata --folder ../braindata_all/ --phase train
+# python3 ./datasets/make_nifti_dataset.py --save_folder ./raid/gbaldini/braindata --folder ../braindata_all/ --phase train
 
 for l in ${lone[@]}; do
   for ll in ${ltwo[@]}; do
     python3 train.py --dataset_mode nifti --model pix2pix3d --name t1t2 --n_epochs 200 --n_epochs_decay 200 \
-    --suffix {model}_{batch_size}_{lambda_L1}_{lambda_L2_T} \
-    --dataroot ./datasets/braindata/ --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0,1,2,3 > "./checkpoints/out_${b}_${l}_${ll}.txt"
+    --lambda_L1 $l --lambda_L2_T $ll --gpu_id 0,1,2,3 --suffix {model}_{batch_size}_{lambda_L1}_{lambda_L2_T} \
+    --dataroot /input --checkpoints_dir /checkpoints > "/checkpoints/out_${b}_${l}_${ll}.txt"
   done
 done
